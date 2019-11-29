c94e17418773a81d24790cdd24f470f4279e86b0
Sarah Drasner <sarah.drasner@gmail.com>
2019-11-28 21:34:12 -0700

    don't use mapState for one property

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -7,11 +7,11 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-
 export default {
   computed: {
-    ...mapState(["posts"]),
+    posts() {
+      return this.$store.state.posts;
+    },
     post() {
       return this.posts.find(el => el.slug === this.slug);
     }
